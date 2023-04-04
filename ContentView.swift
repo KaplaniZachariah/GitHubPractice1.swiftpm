045648f7cb0d34421adeba9f20072c06d77cd708
Alan <amuszynski5253@stu.d214.org>
2023-04-04 14:44:09 -0500

    Commit

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State var int =  ""
     @State var defaultInt = 0
-    @State var Answer = 0
+    @State var Answer = Int()
     var body: some View {
         VStack {
             TextField("Enter Number Here", text: $int)
@@ -12,8 +12,14 @@ struct ContentView: View {
             TextField("Enter Number Here", text: $int)
                 .textFieldStyle(.roundedBorder)
                 .padding()
-        VStack{
-            Button(<#T##title: StringProtocol##StringProtocol#>, action: <#T##() -> Void#>)
+            
+            Button("Add", action: <#T##() -> Void#>)
+            
+            Button("Subtract", action: <#T##() -> Void#>)
+            
+            Button("Multiply", action: <#T##() -> Void#>)
+            
+            Button("Divide", action: <#T##() -> Void#>)
             
             Text(Answer)
         }
